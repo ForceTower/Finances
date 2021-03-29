a4b74823b98ce8cc0546375046824bd4f813fd9d
João Sena <joaopaulo761@gmail.com>
2021-03-29 16:30:03 -0300

    Add toolkit

--- app/src/main/java/dev/forcetower/finances/MainActivity.kt
+++ app/src/main/java/dev/forcetower/finances/MainActivity.kt
@@ -2,10 +2,12 @@ package dev.forcetower.finances
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.forcetower.toolkit.components.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val (added, removed) = listOf(true, false, true, false).partition { it }
     }
 }
